13c3bde4e090a9266006d210cd7e0c1c5802d752
Dhruva <dhruva.gowdastorz@epfl.ch>
2024-01-26 00:13:27 +0100

    potential fix for image caching issue

--- js/pcloader_modular.js
+++ js/pcloader_modular.js
@@ -340,7 +340,11 @@ export function initializePointCloud(containerId, supercontainerId, imageSrcPath
 			fragmentShader: this.get( 'particle-fs' )
 		} );
         // load point cloud
-        loadPC();
+		img.onload = function() {
+			imgSrc.onload = function() {
+        		loadPC();
+			}
+		}
 
 	} );
 	
